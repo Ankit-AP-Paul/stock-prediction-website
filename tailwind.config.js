6760e4fb152828a9abaf7e8784689967775a8c19
Hindol19 <hindolbanerjee5@gmail.com>
2024-01-21 17:45:22 +0530

    Updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,40 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      fontFamily: {
+        mont: ["var(--font-mont)"],
+      },
+      colors: {
+        // dark: "#1b1b1b",
+        dark: "#060608",
+        // light: "#f5f5f5",
+        light: "#fad3cf",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        "2xl": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xl: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lg: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        md: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        sm: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+
+        xs: { max: "479px" },
+        // => @media (max-width: 479px) { ... }
       },
     },
   },
